cc17e0be85a11fa0fd991bf386a8ba4d58b68e40
RealCypher123 <cypher123@mail.com>
2017-08-15 18:42:28 -0400

    Git is hard
    Used dir upload. Yw Mike :)))) Time for my death lololol

--- commands/stories.js
+++ commands/stories.js
@@ -4,8 +4,9 @@ exports.run = (client, message, args) => {
     "<@!154066620124364800> took a 3 week vay-cay because he wanted to escape the wrath of coding me.",
     "Cypher123 got so tired of working on this bot, he hopes to dump it on another person. If only he wasn't so lazy...",
     "\"Once upon a time when I was a youngin' I would always get upset if I held shift while pressing space because I thought I was capitalizing space and that whatever I was typing into wouldn't accept it so I'd backspace and press space normally.\" - <@127184346334494721>",
-    "Once I did a thing, then the world blew up... sad day."]
-    
+    "Once I did a thing, then the world blew up... sad day.",
+    "AaAaAAaaAaA HAhAhAhA gIt Is sOoOoO COoL"]
+
     let num = Math.floor(Math.random() * items.length);
     return message.channel.send("Here's your random story!```"+items[num]+"```")
 }
